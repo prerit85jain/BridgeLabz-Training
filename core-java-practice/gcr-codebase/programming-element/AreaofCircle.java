20d86f9624c0724e016328170e804705486092fd
Prerit Jain <preritjain@gmail.com>
2025-12-17 15:41:22 +0530

    [Prerit Jain] Added 4 problems in gcr-codebase

---
+++ core-java-practice/gcr-codebase/programming-element/AreaofCircle.java
@@ -0,0 +1,9 @@
+public class AreaofCircle {
+    // Main function
+    public static void main(String[] args){
+        double radius = 7;
+        double area = 3.14*radius*radius; // Area calculation
+        System.out.println("Area: "+ area);
+    }
+    
+}
---
+++ core-java-practice/gcr-codebase/programming-element/CtoFConversion.java
@@ -0,0 +1,7 @@
+public class CtoFConversion {
+    public static void main(String[] args) {
+        double celcius = 37;
+        double fahrenheit = (celcius * 9/5) + 32; // Conversion formula
+        System.out.println("In Fahrenheit: " + fahrenheit);
+    }
+}
