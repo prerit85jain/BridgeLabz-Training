fc2a4498ebb037d9cc2d0f90b5dcef225c4ea092
Prerit Jain <preritjain@gmail.com>
2026-01-30 21:28:44 +0530

    [Prerit Jain] add. added Bank Account scenario-based problem.

---
+++ collection-practice/scenario-based/bankAccount/src/main/java/bankAccount/BankAccount.java
@@ -0,0 +1,33 @@
+package bankAccount;
+
+public class BankAccount {
+	private double balance;
+	
+	public BankAccount(double openingAmt) {
+		if(openingAmt < 0) {
+			throw new IllegalArgumentException("Initial amount cannot be negative");
+		}
+		balance = openingAmt;
+	}
+	
+	public void deposite(double amount) {
+		if(amount <= 0) {
+			throw new IllegalArgumentException("Deposite amount should be positive");
+		}
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		if(amount <= 0) {
+			throw new IllegalArgumentException("Withdraw amount should be positive");
+		}else if(amount > balance) {
+			throw new IllegalArgumentException("Insufficient balance");
+		}else {
+			balance -= amount;
+		}
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+}
